64e73d8210b1f236f4cca50b3480dd497dc3854e
Ligia Kaczmarek <75280@ue.poznan.pl>
2023-03-27 16:47:25 +0200

    all commits - techer

---
+++ exercise/ex_3.py
@@ -0,0 +1,13 @@
+# prostokat
+
+from pyrsistent import b
+
+
+a = 10
+b = 5
+obwod  = 2 * a + 2 * b
+pole = a * b
+
+# obrod kawadratu wynosi 40, a pole 100 
+print ("obwod kwadratu wynosi" * str(obwod) + ", pole" + str(pole)+".")
+print(pole)
