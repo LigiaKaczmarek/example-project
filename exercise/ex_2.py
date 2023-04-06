ed9a030ee6f9c9d9cc0fc132d00b08acb3c1594c
Ligia Kaczmarek <75280@ue.poznan.pl>
2023-04-06 18:59:17 +0200

    Dodane kolo

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -1,9 +1,9 @@
-# trapez
-a = 3
-b = 4
-c = 2
-h = 5
-obwod  = 2* a + 2*4 + 2*c
-pole = (a+b)*h/2
-print ("obwod trapezu wynosi" + str(obwod) + ", pole" + str(pole)+".")
+#koło
+pi = 3,14
+r = 5
+obwod  = 2 * pi * r
+pole = pi * r * r
+
+# obrod kawadratu wynosi 40, a pole 100 
+print ("obwod koła wynosi" + str(obwod) + ", pole" + str(pole)+".")
 print(pole)
