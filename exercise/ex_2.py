1ead58b064861f14d56e4a9f5214fed39abe1e26
Mikołaj Kasperczak <73179@student.ue.poznan.pl>
2023-04-06 19:51:07 +0200

    dodany kwadrat

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -8,51 +8,7 @@ pole = a * a
 print ("obwod kwadratu wynosi" + str(obwód) + ", pole" + str(pole)+".")
 print(pole)
 
-#prostokat
-a = 10
-b =5
-obwód  = a * 2 + 2 * b
-pole = a * b
-
-# obrod kawadratu wynosi 40, a pole 100 
-print ("obwod prostokatu wynosi" + str(obwód) + ", pole" + str(pole)+".")
-print(pole)
 
 
-
-# trapez
-a = 3
-b = 4
-c = 2
-h = 5
-obwod  = 2* a + 2*4 + 2*c
-pole = (a+b)*h/2
-print ("obwod trapezu wynosi" + str(obwod) + ", pole" + str(pole)+".")
-print(pole)
-
-
-
-
-#koło
-pi = 3,14
-r = 5
-obwod  = 2 * pi * r
-pole = pi * r * r
-
-# obrod kawadratu wynosi 40, a pole 100 
-print ("obwod koła wynosi" + str(obwod) + ", pole" + str(pole)+".")
-print(pole)
-
-
-#romb
-a = 10
-b = 5
-h = 4
-obwod  = 2 * a + 2 * b
-pole = a * h/2
-
-# obrod kawadratu wynosi 40, a pole 100 
-print ("obwod rombu wynosi" + str(obwod) + ", pole" + str(pole)+".")
-print(pole)
 
 
