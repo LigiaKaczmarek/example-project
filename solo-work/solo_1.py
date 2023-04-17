8c9236851697ffc2b52ddad008680c55ee29a224
Ligia Kaczmarek <75280@ue.poznan.pl>
2023-04-17 20:35:48 +0200

    Zadanie 1.9

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -66,3 +66,16 @@ for student in posortowani_studenci:
     print(student)
 
 
+# zadanie 1.9
+
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+
+liczba_n = 0
+for student in studenci:
+    nazwisko = student.split()[-1] 
+    if nazwisko.startswith('N'):
+        liczba_n += 1
+
+print("Liczba studentow na N wynosi:", liczba_n)
+
+
