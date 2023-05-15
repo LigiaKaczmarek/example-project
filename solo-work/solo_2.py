2031625ce649a57be5ab68d7b5a4651e13793b83
Ligia Kaczmarek <75280@ue.poznan.pl>
2023-05-15 12:11:57 +0200

    zadanie na zajeciach prostokat

--- solo-work/solo_2.py
+++ solo-work/solo_2.py
@@ -19,6 +19,18 @@ trojkat_rownoboczny = Trojkat(10, 10, 10, 8)
 trojkat_ligii = Trojkat(8, 6, 10,4)
 print(trojkat_rownoboczny.obwod())
 
+class Prostokat:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        #self obwod
+    def obwod(self):
+        return 2 * self.a + 2 * self.b
+    def pole(self):
+        return self.a * self.b
+prostokat_ligii = Prostokat(5, 10)
+print(prostokat_ligii.obwod())
+print(prostokat_ligii.pole())
  
 
 
